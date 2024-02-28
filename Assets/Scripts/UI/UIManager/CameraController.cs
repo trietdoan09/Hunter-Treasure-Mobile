7fb10aab9a9e6dd3d2231bfd419953a4c11ab119
trietdoan09 <92006097+trietdoan09@users.noreply.github.com>
2024-02-28 19:45:23 +0700

    Update combat system

--- Assets/Scripts/UI/UIManager/CameraController.cs
+++ Assets/Scripts/UI/UIManager/CameraController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-    public GameObject Player;
+    private PlayerMovement player;
+    private void Start()
+    {
+        //player = FindObjectOfType<PlayerMovement>();
+    }
     void Update()
     {
-        transform.position = Player.transform.position + new Vector3(0, 3.5f, -10);
+        //transform.position = player.transform.position + new Vector3(0, 3.5f, -10);
     }
 }
