74f496b77dd7d00a5003dfd90d8ff4c84569a9b1
trietdoan09 <92006097+trietdoan09@users.noreply.github.com>
2024-02-28 20:33:44 +0700

    Update combat system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,7 +13,6 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 5f;
         rigidbody2D = GetComponent<Rigidbody2D>();
         joystick = FindObjectOfType<Joystick>();
         animator = GetComponent<Animator>();
@@ -22,7 +21,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetFloat("speed", moveSpeed);
     }
     private void FixedUpdate()
     {
@@ -33,11 +32,25 @@ public class PlayerMovement : MonoBehaviour
     {
         if(joystick.joystickVec.x != 0)
         {
+            moveSpeed = 5f;
             rigidbody2D.velocity = new Vector2(joystick.joystickVec.x * moveSpeed, 0);
+            ScaleFace();
         }
         else
         {
             rigidbody2D.velocity = Vector2.zero;
+            moveSpeed = 0;
+        }
+    }
+    private void ScaleFace()
+    {
+        if(joystick.joystickVec.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1);
         }
     }
 }
