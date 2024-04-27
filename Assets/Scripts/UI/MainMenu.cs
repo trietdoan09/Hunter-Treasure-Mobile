18e0a912b3cc6b13e96b0a0a91ef79344794bb69
trietdoan09 <92006097+trietdoan09@users.noreply.github.com>
2024-04-27 11:00:30 +0700

    completed main scene

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -14,5 +15,13 @@ public class MainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+    public void PlayGameBtn()
+    {
+        SceneManager.LoadScene("ChooseCharacter");
+    }
+    public void ExitGameBtn()
+    {
+        Application.Quit();
     }
 }
